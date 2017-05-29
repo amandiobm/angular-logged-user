860f84453409540dbcb5568e2235ecfdbffe5e89
Amandio Magalhaes <amandio.magalhaes@gmail.com>
2017-05-29 00:41:31 -0300

    Cleaning up

--- angular-logged-user.js
+++ angular-logged-user.js
@@ -1,335 +1,178 @@
 'use strict';
 
-var NG_HIDE_CLASS = 'ng-hide';
-
 angular.module('svdesignti.loggedUser', []);
 
 angular.module('svdesignti.loggedUser').provider('LoggedUserService', [
-  function () {
+    function () {
 
-    /**
-     * Polyfill for IE8
-     *
-     * http://stackoverflow.com/a/1181586
-     */
-    if (!Array.prototype.indexOf) {
-      Array.prototype.indexOf = function (needle) {
-        var l = this.length;
-        for (; l--;) {
-          if (this[l] === needle) {
-            return l;
-          }
+        /**
+         * Polyfill for IE8
+         *
+         * http://stackoverflow.com/a/1181586
+         */
+        if (!Array.prototype.indexOf) {
+            Array.prototype.indexOf = function (needle) {
+                var l = this.length;
+                for (; l--;) {
+                    if (this[l] === needle) {
+                        return l;
+                    }
+                }
+                return -1;
+            };
         }
-        return -1;
-      };
+
+        var config = {
+            storage: 'sessionStorage',
+            storageKey: 'LoggedUserService'
+        };
+
+        var data = {
+            user: {}
+        };
+
+        /**
+         * Retrieve the user
+         *
+         * @returns {object}
+         */
+        var getUser = function () {
+            console.log('src/angular-logged-user.js:40', data);
+            return data.user;
+        };
+
+        /**
+         * Persist data to storage based on config
+         */
+        var save = function () {
+            switch (config.storage) {
+                case 'sessionStorage':
+                    saveToStorage('sessionStorage');
+                    break;
+                case 'localStorage':
+                    saveToStorage('localStorage');
+                    break;
+                default:
+                    // Don't save
+                    return;
+            }
+        };
+
+        var unset = function () {
+            switch (config.storage) {
+                case 'sessionStorage':
+                    unsetFromStorage('sessionStorage');
+                    break;
+                case 'localStorage':
+                    unsetFromStorage('localStorage');
+                    break;
+                default:
+                    // Don't save
+                    return;
+            }
+        };
+
+        /**
+         * Persist data to web storage
+         */
+        var saveToStorage = function (storagetype) {
+            window[storagetype].setItem(config.storageKey, JSON.stringify(data));
+        };
+
+        /**
+         * Unset data from web storage
+         */
+        var unsetFromStorage = function (storagetype) {
+            window[storagetype].removeItem(config.storageKey);
+        };
+
+        /**
+         * Retrieve data from web storage
+         */
+        var fetchFromStorage = function (storagetype) {
+            var data = window[storagetype].getItem(config.storageKey);
+            return (data) ? JSON.parse(data) : false;
+        };
+
+        var LoggedUserService = {};
+        LoggedUserService.resume = resume;
+
+
+        /**
+         * Restore data from web storage.
+         *
+         * Returns true if web storage exists and false if it doesn't.
+         *
+         * @returns {boolean}
+         */
+        function resume() {
+            var storedData;
+
+            switch (config.storage) {
+                case 'sessionStorage':
+                    storedData = fetchFromStorage('sessionStorage');
+                    break;
+                case 'localStorage':
+                    storedData = fetchFromStorage('localStorage');
+                    break;
+                default:
+                    storedData = null;
+            }
+            if (storedData) {
+                angular.extend(data, storedData);
+                return true;
+            }
+
+            return false;
+        }
+
+        /**
+         * Remove data from web storage
+         */
+        LoggedUserService.flushStorage = function () {
+            unset();
+        };
+
+        /**
+         * Returns the current user
+         * @returns {Object}
+         */
+        LoggedUserService.getUser = function () {
+            console.log('src/angular-logged-user.js:152', data);
+            return data.user;
+        };
+
+        /**
+         * Set the user object (overwriting previous user)
+         *
+         * @param user
+         */
+        LoggedUserService.setUser = function (user) {
+            console.log('src/angular-logged-user.js:162', user);
+            data.user = user;
+            console.log('src/angular-logged-user.js:164', data);
+            save();
+        };
+
+        /**
+         * Get logged user
+         *
+         * @returns {Object}
+         */
+        LoggedUserService.get = function () {
+            console.log('src/angular-logged-user.js:174');
+            return getUser();
+        };
+
+        return {
+            config: function (userConfig) {
+                angular.extend(config, userConfig);
+            },
+            resume: resume,
+            $get: function () {
+                return LoggedUserService;
+            }
+        };
+
     }
-
-    var config = {
-      storage: 'sessionStorage',
-      storageKey: 'LoggedUserService'
-    };
-
-    var data = {
-      roles: [],
-      abilities: {}
-    };
-
-    /**
-     * Does the given role have abilities granted to it?
-     *
-     * @param role
-     * @returns {boolean}
-     */
-    var roleHasAbilities = function (role) {
-      return (typeof data.abilities[role] === 'object');
-    };
-
-    /**
-     * Retrieve the abilities array for the given role
-     *
-     * @param role
-     * @returns {Array}
-     */
-    var getRoleAbilities = function (role) {
-      return (roleHasAbilities(role)) ? data.abilities[role] : [];
-    };
-
-    /**
-     * Persist data to storage based on config
-     */
-    var save = function () {
-      switch (config.storage) {
-        case 'sessionStorage':
-          saveToStorage('sessionStorage');
-          break;
-        case 'localStorage':
-          saveToStorage('localStorage');
-          break;
-        default:
-          // Don't save
-          return;
-      }
-    };
-
-    var unset = function () {
-      switch (config.storage) {
-        case 'sessionStorage':
-          unsetFromStorage('sessionStorage');
-          break;
-        case 'localStorage':
-          unsetFromStorage('localStorage');
-          break;
-        default:
-          // Don't save
-          return;
-      }
-    };
-
-    /**
-     * Persist data to web storage
-     */
-    var saveToStorage = function (storagetype) {
-      window[storagetype].setItem(config.storageKey, JSON.stringify(data));
-    };
-
-    /**
-     * Unset data from web storage
-     */
-    var unsetFromStorage = function (storagetype) {
-      window[storagetype].removeItem(config.storageKey);
-    };
-
-    /**
-     * Retrieve data from web storage
-     */
-    var fetchFromStorage = function (storagetype) {
-      var data = window[storagetype].getItem(config.storageKey);
-      return (data) ? JSON.parse(data) : false;
-    };
-
-    var LoggedUserService = {};
-    LoggedUserService.resume = resume;
-
-
-    /**
-     * Restore data from web storage.
-     *
-     * Returns true if web storage exists and false if it doesn't.
-     *
-     * @returns {boolean}
-     */
-    function resume() {
-      var storedData;
-
-      switch (config.storage) {
-        case 'sessionStorage':
-          storedData = fetchFromStorage('sessionStorage');
-          break;
-        case 'localStorage':
-          storedData = fetchFromStorage('localStorage');
-          break;
-        default:
-          storedData = null;
-      }
-      if (storedData) {
-        angular.extend(data, storedData);
-        return true;
-      }
-
-      return false;
-    }
-
-    /**
-     * Remove data from web storage
-     */
-    LoggedUserService.flushStorage = function () {
-      unset();
-    };
-
-    /**
-     * Attach a role to the current user
-     *
-     * @param role
-     */
-    LoggedUserService.attachRole = function (role) {
-      if (data.roles.indexOf(role) === -1) {
-        data.roles.push(role);
-        save();
-      }
-    };
-
-    /**
-     * Remove role from current user
-     *
-     * @param role
-     */
-    LoggedUserService.detachRole = function (role) {
-      var i = data.roles.indexOf(role);
-      if (i > -1) {
-        data.roles.splice(i, 1);
-        save();
-      }
-    };
-
-    /**
-     * Remove all roles from current user
-     */
-    LoggedUserService.flushRoles = function () {
-      data.roles = [];
-      save();
-    };
-
-    /**
-     * Check if the current user has role(s) attached
-     *
-     * @param role
-     * @returns {boolean}
-     */
-    LoggedUserService.hasRole = function (role) {
-      var roles = angular.isArray(role) ? role : [role];
-      for (var l = roles.length; l--;) {
-        if (data.roles.indexOf(roles[l]) === -1) {
-          return false;
-        }
-      }
-      return !!roles.length;
-    };
-
-    /**
-     * Check if the current user any of the given roles
-     *
-     * @param roles
-     * @returns {boolean}
-     */
-    LoggedUserService.hasAnyRole = function (roles) {
-      for (var l = roles.length; l--;) {
-        if (LoggedUserService.hasRole(roles[l])) {
-          return true;
-        }
-      }
-      return false;
-    };
-
-    /**
-     * Returns the current user roles
-     * @returns {Array}
-     */
-    LoggedUserService.getRoles = function () {
-      return data.roles;
-    };
-
-    /**
-     * Set the abilities object (overwriting previous abilities)
-     *
-     * Each property on the abilities object should be a role.
-     * Each role should have a value of an array. The array should contain
-     * a list of all of the roles abilities.
-     *
-     * Example:
-     *
-     *    {
-     *        guest: ['login'],
-     *        user: ['logout', 'view_content'],
-     *        admin: ['logout', 'view_content', 'manage_users']
-     *    }
-     *
-     * @param abilities
-     */
-    LoggedUserService.setAbilities = function (abilities) {
-      data.abilities = abilities;
-      save();
-    };
-
-    /**
-     * Add an ability to a role
-     *
-     * @param role
-     * @param ability
-     */
-    LoggedUserService.addAbility = function (role, ability) {
-      if (!data.abilities[role]) {
-        data.abilities[role] = [];
-      }
-      data.abilities[role].push(ability);
-      save();
-    };
-
-    /**
-     * Does current user have permission to do something?
-     *
-     * @param ability
-     * @returns {boolean}
-     */
-    LoggedUserService.can = function (ability) {
-      var role, abilities;
-      // Loop through roles
-      var l = data.roles.length;
-      for (; l--;) {
-        // Grab the the current role
-        role = data.roles[l];
-        abilities = getRoleAbilities(role);
-        if (abilities.indexOf(ability) > -1) {
-          // Ability is in role abilities
-          return true;
-        }
-      }
-      // We made it here, so the ability wasn't found in attached roles
-      return false;
-    };
-
-    /**
-     * Does current user have any of the required permission to do something?
-     *
-     * @param abilities [array]
-     * @returns {boolean}
-     */
-    LoggedUserService.canAny = function (abilities) {
-      var role, roleAbilities;
-      // Loop through roles
-      var l = data.roles.length;
-      var j = abilities.length;
-
-      for (; l--;) {
-        // Grab the the current role
-        role = data.roles[l];
-        roleAbilities = getRoleAbilities(role);
-
-        for (; j--;) {
-          if (roleAbilities.indexOf(abilities[j]) > -1) {
-            // Ability is in role abilities
-            return true;
-          }
-        }
-      }
-      // We made it here, so the ability wasn't found in attached roles
-      return false;
-    };
-
-    return {
-      config: function (userConfig) {
-        angular.extend(config, userConfig);
-      },
-      resume: resume,
-      $get: function () {
-        return LoggedUserService;
-      }
-    };
-
-  }
-]).directive('aclShow', function (LoggedUserService) {
-  return {
-    restrict: 'A',
-    link: function (scope, element, attrs) {
-      scope.$watch(attrs.aclShow, function aclShowWatchAction(value) {
-        var permissions, can;
-        permissions = value.split(',');
-        can = LoggedUserService.canAny(permissions);
-        if (!can) {
-          element.addClass(NG_HIDE_CLASS);
-        } else {
-          element.removeClass(NG_HIDE_CLASS);
-        }
-      });
-    }
-  };
-});
+]);
--- test/unit/angular-logged-user_test.js
+++ test/unit/angular-logged-user_test.js
@@ -1,144 +1,39 @@
 'use strict';
 
 describe('LoggedUserService', function () {
-  var LoggedUserService;
+    var LoggedUserService;
 
-  beforeEach(module('svdesignti.loggedUser'));
+    beforeEach(module('svdesignti.loggedUser'));
 
-  beforeEach(inject(function (_LoggedUserService_) {
-    LoggedUserService = _LoggedUserService_;
-  }));
+    beforeEach(inject(function (_LoggedUserService_) {
+        LoggedUserService = _LoggedUserService_;
+    }));
 
-  describe('roleHasAbilities()', function () {
+    describe('getUser()', function () {
 
-    it('should return true data object has role as a property with an array value', function () {
-      expect(LoggedUserService._roleHasAbilities('foo')).toBeFalsy();
-      LoggedUserService._data.abilities.foo = [];
-      expect(LoggedUserService._roleHasAbilities('foo')).toBeTruthy();
+        it('should return the user in current session', function () {
+            var user = {
+                username: 'foo',
+                displayName: 'bar'
+            };
+            LoggedUserService._data.user = user;
+            expect(LoggedUserService.getUser()).toEqual(user);
+        });
+
     });
 
-  });
+    describe('setUser()', function () {
 
-  describe('getRoleAbilities()', function () {
+        it('should set the user', function () {
+            var user = {
+                username: 'foo',
+                displayName: 'bar'
+            };
+            expect(LoggedUserService._data.user).toEqual({});
+            LoggedUserService.setUser(user);
+            expect(LoggedUserService._data.user).toEqual(user);
+        });
 
-    it('should return the role abilities array', function () {
-      LoggedUserService._data.abilities.foo = ['bar', 'baz'];
-      var abilities = LoggedUserService._getRoleAbilities('foo');
-      expect(abilities).toEqual(['bar', 'baz']);
     });
-
-    it('should return empty array when role does not exist', function () {
-      var abilities = LoggedUserService._getRoleAbilities('foo');
-      expect(abilities).toEqual([]);
-    });
-
-  });
-
-  describe('attachRole()', function () {
-
-    it('should add role to current session', function () {
-      expect(LoggedUserService._data.roles).toEqual([]);
-      LoggedUserService.attachRole('foo');
-      expect(LoggedUserService._data.roles).toEqual(['foo']);
-    });
-
-    it('should add role only once', function () {
-      expect(LoggedUserService._data.roles).toEqual([]);
-      LoggedUserService.attachRole('foo');
-      LoggedUserService.attachRole('foo');
-      LoggedUserService.attachRole('foo');
-      expect(LoggedUserService._data.roles).toEqual(['foo']);
-    });
-
-  });
-
-  describe('detachRole()', function () {
-
-    it('should remove role to current session', function () {
-      LoggedUserService._data.roles = ['foo'];
-      LoggedUserService.detachRole('foo');
-      expect(LoggedUserService._data.roles).toEqual([]);
-    });
-
-    it('should not throw an error if role does not exist', function () {
-      expect(LoggedUserService._data.roles).toEqual([]);
-      LoggedUserService.detachRole('foo');
-      expect(LoggedUserService._data.roles).toEqual([]);
-    });
-
-  });
-
-  describe('flushRole()', function () {
-
-    it('should reset roles to an empty array', function () {
-      LoggedUserService._data.roles = ['foo', 'bar', 'baz'];
-      expect(LoggedUserService._data.roles).toEqual(['foo', 'bar', 'baz']);
-      LoggedUserService.flushRoles();
-      expect(LoggedUserService._data.roles).toEqual([]);
-    });
-
-  });
-
-  describe('hasRole()', function () {
-
-    it('should return true if role is in current session', function () {
-      LoggedUserService._data.roles = ['foo'];
-      expect(LoggedUserService.hasRole('foo')).toBeTruthy();
-    });
-
-    it('should return true if all roles are in current session', function () {
-      LoggedUserService._data.roles = ['foo', 'bar'];
-      expect(LoggedUserService.hasRole(['foo', 'bar'])).toBeTruthy();
-    });
-
-    it('should return false if role is not in current session', function () {
-      LoggedUserService._data.roles = [];
-      expect(LoggedUserService.hasRole('foo')).toBeFalsy();
-    });
-
-    it('should return false if all roles are not in current session', function () {
-      LoggedUserService._data.roles = ['foo', 'bar'];
-      expect(LoggedUserService.hasRole(['foo', 'bar', 'baz'])).toBeFalsy();
-    });
-
-  });
-
-  describe('hasAnyRole()', function () {
-
-    it('should return true if role is in current session', function () {
-      LoggedUserService._data.roles = ['foo'];
-      expect(LoggedUserService.hasAnyRole(['foo'])).toBeTruthy();
-    });
-
-    it('should return true if only some roles are in current session', function () {
-      LoggedUserService._data.roles = ['foo'];
-      expect(LoggedUserService.hasAnyRole(['foo', 'bar'])).toBeTruthy();
-    });
-
-    it('should return false if role is not in current session', function () {
-      LoggedUserService._data.roles = [];
-      expect(LoggedUserService.hasAnyRole(['foo'])).toBeFalsy();
-    });
-
-  });
-
-  describe('getRoles()', function () {
-
-    it('should return all the roles in current session', function () {
-      LoggedUserService._data.roles = ['foo', 'bar'];
-      expect(LoggedUserService.getRoles()).toEqual(['foo', 'bar']);
-    });
-
-  });
-
-  describe('setAbilities()', function () {
-
-    it('should set given param to abilities', function () {
-      expect(LoggedUserService._data.abilities).not.toEqual('foo');
-      LoggedUserService.setAbilities('foo');
-      expect(LoggedUserService._data.abilities).toEqual('foo');
-    });
-
-  });
 
 });
